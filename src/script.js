7283ad5f5f721d9f6678cce96c87ba2deb312d02
Razvan <razvanbugoi@gmail.com>
2021-06-26 13:55:57 +0100

    added 4 basic functions in script.js : add, substract, multiply and divide

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+	return arr.reduce((a,b) => a + b, 0);
+};
+
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b)
+};
