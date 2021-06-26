01a5bae1813d0476a78273910480d7db11d871ad
Razvan <razvanbugoi@gmail.com>
2021-06-26 16:47:34 +0100

    added a new function called operate that takes an operator and 2 numbers and calls one of the above functions

--- src/script.js
+++ src/script.js
@@ -2,14 +2,35 @@ const add = function(a, b) {
 	return a + b;
 };
 
-const subtract = function(a, b) {
+const substract = function(a, b) {
 	return a - b;
 };
 
-const sum = function(arr) {
-	return arr.reduce((a,b) => a + b, 0);
+const divide = function(a, b) {
+	return a / b;
 };
 
-const multiply = function(arr) {
-  return arr.reduce((a,b) => a * b)
+const multiply = function(a, b) {
+  return a * b;
 };
+
+const operate = function(operator, no1, no2) {
+    switch (operator) {
+        case '+': return add(no1, no2);
+        case '-': return substract(no1, no2);
+        case '*': return multiply(no1, no2);
+        case '/' : return divide(no1, no2);
+        default: 
+        return 'I don\'t recognize that operator'
+    }
+}
+
+// const operate = function(operator, no1, no2) {
+//     let obj = {
+//         '+': add(no1, no2),
+//         '-': subtract(no1, no2),
+//         '/': divide(no1, no2),
+//         '*': multiply(no1, no2)
+//     };
+//     obj[operator]();
+// }
