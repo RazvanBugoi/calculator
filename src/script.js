429346ca3ea5c2064cbd65ecbe11a1ca794dda5a
Razvan <razvanbugoi@gmail.com>
2021-06-27 07:04:58 +0100

    added event listeners for operator functions

--- src/script.js
+++ src/script.js
@@ -1,3 +1,42 @@
+const buttons = document.querySelectorAll('.button')
+const displayNumbers = document.getElementById('current-numbers')
+const history = document.getElementById('calculator-history')
+const operators = document.querySelectorAll('.operator')
+const result = document.getElementById('equal')
+let displayOutput
+let historyOutput
+let currentOperatorSelected;
+
+buttons.forEach((el) => el.addEventListener('click', function(){
+    displayNumbers.textContent += el.textContent
+    displayOutput = displayNumbers.textContent;
+}))
+
+operators.forEach((el) => el.addEventListener('click', function() {
+    if (!displayNumbers.textContent) {
+        displayNumbers.textContent = 'You need to select a number first!'
+    }
+    history.textContent = displayOutput;
+    displayNumbers.textContent = '';
+    currentOperatorSelected = el.textContent;
+    historyOutput = displayOutput;
+}))
+
+
+window.addEventListener("keydown", function(event) {
+    if (event.key == "Enter") {
+        const no1 = Number(displayOutput.match(/\d/g).join(''));
+        const no2 = Number(historyOutput.match(/\d/g).join(''))
+        history.textContent = operate(currentOperatorSelected, no1, no2)
+        displayNumbers.textContent = ''
+    }
+    return
+  }, true);
+
+result.addEventListener('keydown', function(event) {
+    console.log(event.code)
+})
+
 const add = function(a, b) {
 	return a + b;
 };
@@ -24,13 +63,3 @@ const operate = function(operator, no1, no2) {
         return 'I don\'t recognize that operator'
     }
 }
-
-// const operate = function(operator, no1, no2) {
-//     let obj = {
-//         '+': add(no1, no2),
-//         '-': subtract(no1, no2),
-//         '/': divide(no1, no2),
-//         '*': multiply(no1, no2)
-//     };
-//     obj[operator]();
-// }
